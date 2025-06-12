fc95a46c0040afe69e44477a2e3d28055750acc8
Husam Al-Dhaheri <109308829+hdsm55@users.noreply.github.com>
2025-06-12 23:50:01 +0300

    Updated seed.ts

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -3,6 +3,10 @@ import dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
 dotenv.config();
 
+
+console.log('SUPABASE_URL=', process.env.SUPABASE_URL);
+console.log('SERVICE_ROLE=', process.env.SUPABASE_SERVICE_ROLE_KEY);
+
 const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
